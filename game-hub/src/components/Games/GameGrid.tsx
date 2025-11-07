f11cbd46a63aa62436861f55433d2fcdde2cc9af
Frank Ahiadome <frank.ahiadome@hubtel.com>
2025-11-07 08:55:21 +0000

    created custom hook to fetch games

--- game-hub/src/components/Games/GameGrid.tsx
+++ game-hub/src/components/Games/GameGrid.tsx
@@ -1,35 +1,7 @@
-import apiClient from '@/Services/api-client';
-import React, { useEffect, useState } from 'react';
-
-interface Game {
-  id: number;
-  name: string;
-  image: string;
-  rating: number;
-  released: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Game[];
-}
+import useGames from '@/hooks/useGames';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>('/games')
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  }, [error]);
+  const { games, error } = useGames();
 
   return (
     <div>
---
+++ game-hub/src/hooks/useGames.ts
@@ -0,0 +1,40 @@
+import apiClient from '@/Services/api-client';
+import { CanceledError } from 'axios';
+import { useEffect, useState } from 'react';
+
+interface Game {
+  id: number;
+  name: string;
+  image: string;
+  rating: number;
+  released: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+export default useGames;
