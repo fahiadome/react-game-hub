81a381dd4ec018537f81825e53a035dab994363d
Frank Ahiadome <frank.ahiadome@hubtel.com>
2025-11-10 16:15:12 +0000

    clean up genre display

--- game-hub/src/components/Genres/GenreList.tsx
+++ game-hub/src/components/Genres/GenreList.tsx
@@ -21,39 +21,48 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   if (error) return <Text>{error}</Text>;
   if (isLoading) return <Spinner />;
   return (
-    <List.Root>
-      {data?.results.map((genre) => (
-        <ListItem key={genre.id} paddingY="2px" cursor="pointer">
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background) || ''}
-              alt={genre.name}
-              boxSize="32px"
-              borderRadius="6px"
-              objectFit="cover"
-            />
-            <Button
-              colorPalette={selectedGenre?.id === genre.id ? 'green' : 'gray'}
-              fontSize="md"
-              textAlign="left"
-              onClick={() => {
-                onSelectGenre(genre);
-              }}
-            >
-              {genre.name}{' '}
-              <Badge
-                colorPalette="green"
-                fontSize="10px"
-                paddingX={1}
-                borderRadius="full"
+    <>
+      <Text fontSize="2xl" fontWeight="bold" marginBottom={5}>
+        Genres
+      </Text>
+
+      <List.Root>
+        {data?.results.map((genre) => (
+          <ListItem key={genre.id} paddingY="2px" cursor="pointer">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background) || ''}
+                alt={genre.name}
+                boxSize="32px"
+                borderRadius="6px"
+                objectFit="cover"
+              />
+              <Button
+                whiteSpace="normal"
+                // textOverflow="ellipsis"
+                // overflow="hidden"
+                colorPalette={selectedGenre?.id === genre.id ? 'green' : 'gray'}
+                fontSize="md"
+                textAlign="left"
+                onClick={() => {
+                  onSelectGenre(genre);
+                }}
               >
-                {genre.games_count}
-              </Badge>
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List.Root>
+                {genre.name}{' '}
+                <Badge
+                  colorPalette="green"
+                  fontSize="10px"
+                  paddingX={1}
+                  borderRadius="full"
+                >
+                  {genre.games_count}
+                </Badge>
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
